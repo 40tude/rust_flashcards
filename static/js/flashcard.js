c54b8c66634c608df7100e43b701ab95e9310125
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-11-27 15:54:26 +0100

    Make TAB working in card (to go back to landing page with keyboard)

--- static/js/flashcard.js
+++ static/js/flashcard.js
@@ -2,6 +2,7 @@
 (function() {
     const answerDiv = document.getElementById('answer-content');
     const actionBtn = document.getElementById('action-btn');
+    const backBtn = document.getElementById('back-btn');
     const isPngOnly = actionBtn.dataset.pngOnly === 'true';
 
     let isRevealed = isPngOnly;
@@ -32,9 +33,14 @@
         }
     });
 
-    // ENTER key
+    // ENTER key - only intercept if back button doesn't have focus
     document.addEventListener('keydown', function(e) {
         if (e.key === 'Enter') {
+            // If back button has focus, let browser follow the link
+            if (document.activeElement === backBtn) {
+                return;
+            }
+
             e.preventDefault();
             handleAction();
         }
